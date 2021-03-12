8132247c8096fd99e8c9bd8ac353b1c0125439ca
Dayo Okesola <dayokesola@hotmail.com>
2021-03-12 22:49:46 +0100

    add book

--- main.go
+++ main.go
@@ -85,10 +85,14 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 func addBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("add a book")
 	var book Book
+	var id int
 	_ = json.NewDecoder(r.Body).Decode(&book)
-	books = append(books, book)
 
-	json.NewEncoder(w).Encode(books)
+	err := db.QueryRow("insert into books (title, author, year) values($1,$2,$3) returning id;",
+		book.Title, book.Author, book.Year).Scan(&id)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(book)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
